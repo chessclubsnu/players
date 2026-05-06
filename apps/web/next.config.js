4b26033670828968f48f2b57101d5f80e8fcf3d4
Garchomp <chessclubsnu@gmail.com>
2026-05-06 09:41:28 +0900

    backup

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,11 +3,7 @@ const nextConfig = {
   typescript: {
     // !! 주의: 타입 에러가 있어도 빌드를 강제로 완료합니다.
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // !! 주의: ESLint 에러가 있어도 빌드를 강제로 완료합니다.
-    ignoreDuringBuilds: true,
-  },
+  }
 };
 
 export default nextConfig;
